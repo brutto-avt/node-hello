58e23b111025f1640f5b3d8e71188cb35cb0275f
bruttoavt@bitbucket.org <lcn.andre@gmail.com>
2016-01-11 21:48:25 -0200

    Port changed to 80

--- app.js
+++ app.js
@@ -5,5 +5,5 @@ app.get('/', function (req, res) {
   res.send('<html><body>Hello from Node.js!</body></html>');
 });
 
-app.listen(8080);
+app.listen(80);
 console.log('Running on http://localhost:8080');
